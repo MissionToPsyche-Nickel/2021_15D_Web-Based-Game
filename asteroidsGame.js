9f2e406d7951e0096321d2a7f03728ce68d9709a
Danial Yunus <dyunus@asu.edu>
2020-10-18 16:44:45 -0700

    space background

--- asteroidsGame.js
+++ asteroidsGame.js
@@ -1,9 +1,17 @@
 console.log("ashfykjldsajkds")
 
-function setup(){
-    createCanvas(500,500)
-}
+document.getElementById("myCanvas").style.background = "url('svg/Space_Background.png')";
 
-function draw(){
-    background(51)
+//show_image("svg/Character.svg",100,100,1)
+//show_image("svg/Valid_Asteroid.svg",100,100,1)
+//show_image("svg/Error_Asteroid.svg",100,100,1)
+
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
 }
