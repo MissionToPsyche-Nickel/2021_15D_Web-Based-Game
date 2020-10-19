b72d241da9691f90e47b81844bb726338dc62e96
Sophia Shovkovy <sophia.shovkovy@culdesac.com>
2020-10-18 17:08:38 -0700

    Rename folder svg to images

--- asteroidsGame.js
+++ asteroidsGame.js
@@ -1,17 +1,18 @@
-console.log("ashfykjldsajkds")
+console.log("ashfykjldsajkds");
 
-document.getElementById("myCanvas").style.background = "url('svg/Space_Background.png')";
+document.getElementById("myCanvas").style.background =
+  "url('images/Space_Background.png')";
 
 //show_image("svg/Character.svg",100,100,1)
 //show_image("svg/Valid_Asteroid.svg",100,100,1)
 //show_image("svg/Error_Asteroid.svg",100,100,1)
 
 function show_image(src, width, height, alt) {
-    var img = document.createElement("img");
-    img.src = src;
-    img.width = width;
-    img.height = height;
-    img.alt = alt;
-    // This next line will just add it to the <body> tag
-    document.body.appendChild(img);
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+  // This next line will just add it to the <body> tag
+  document.body.appendChild(img);
 }
