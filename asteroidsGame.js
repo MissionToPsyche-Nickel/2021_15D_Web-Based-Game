f13f81be0fc1013f3c9337cabd4e1333f5766d25
Sophia Shovkovy <sophia.shovkovy@culdesac.com>
2020-10-19 20:22:46 -0700

    Change background

--- asteroidsGame.js
+++ asteroidsGame.js
@@ -2,17 +2,17 @@
 
 //Set the canvas background to be the Space_Background photo
 document.getElementById("spaceCanvas").style.background =
-  "url('images/Space_Background.png ')";
+  "url('images/Space_Background_V2.png ')";
 
-console.log("Starting...."); //sanity check console log 
+console.log("Starting...."); //sanity check console log
 
 //GLOBAL VARIABLES:
 var asteroids = []; //2D Array of integers (representing asteroids) where "1" is a Valid_Asteroid (Gray) and "-1" is an Error_Asteroid (Red)
 var score = 0; //current score
-var lives = 3; //remaining lives 
-var currentPosition = 0; //the robot's current position, 0 is left col, 1 is middle col, 2 is right col 
+var lives = 3; //remaining lives
+var currentPosition = 0; //the robot's current position, 0 is left col, 1 is middle col, 2 is right col
 
-//Populates the asteroids 2D array with data 
+//Populates the asteroids 2D array with data
 initializeAsteroids();
 
 //Main Display Function
@@ -26,14 +26,14 @@ function handleKeyPress(event) {
   //If "Left Arrow" or "A" are pressed
   if (event.keyCode == 37 || event.keyCode == 65) {
     event.preventDefault(); //Disable default keyboard behavior (scrolling)
-    console.log("Left/A was pressed"); //Debugging Statement 
+    console.log("Left/A was pressed"); //Debugging Statement
     if (currentPosition >= 1) currentPosition = currentPosition - 1; //move the robot's current position one column to the left only if it is a valid move
     showRobot(); //updates robot's position
-  } 
-   //If "Right Arrow" or "D" are pressed
+  }
+  //If "Right Arrow" or "D" are pressed
   else if (event.keyCode == 39 || event.keyCode == 68) {
     event.preventDefault(); //Disable default keyboard behavior (scrolling)
-    console.log("Right/D was pressed"); //Debugging Statement 
+    console.log("Right/D was pressed"); //Debugging Statement
     if (currentPosition <= 1) currentPosition = currentPosition + 1; //move the robot's current position one column to the right only if it is a valid move
     showRobot(); //updates robot's position
   }
@@ -45,7 +45,7 @@ function handleKeyPress(event) {
     calcJump(); //Calculate Game State after the robot's "jump"
     shiftAsteroidsDown(); //Shifts asteroids down and adds a new row
     showAsteroids(); //Displays new game state
-  } 
+  }
 }
 
 function calcJump() {
@@ -53,19 +53,17 @@ function calcJump() {
     score++;
     document.getElementById("score").innerHTML = "SCORE: " + score;
     console.log("GOOD JUMP!");
-  } 
-  else {
+  } else {
     lives--;
     console.log("UNSAFE ASTEROID JUMP!");
   }
   if (lives == 0) {
     showHearts();
-    setTimeout(function() {
-      if(confirm("GAME OVER! Play Again?")){
+    setTimeout(function () {
+      if (confirm("GAME OVER! Play Again?")) {
         initializeAsteroids();
         document.getElementById("score").innerHTML = "SCORE: " + score;
-      }
-      else{
+      } else {
         document.removeEventListener("keydown", handleKeyPress);
       }
     }, 50); //waits 50ms to update heart drawing on canvas before displaying alert
@@ -79,7 +77,7 @@ function initializeAsteroids() {
   for (var i = 0; i < 3; i++) {
     asteroids.push(getAsteroidRow());
   }
-  asteroids.push([1,1,1]);
+  asteroids.push([1, 1, 1]);
   const canvas = document.getElementById("spaceCanvas");
   var ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -90,7 +88,7 @@ function initializeAsteroids() {
 function getAsteroidRow() {
   var asteroidRow = [1, 1, 1];
   asteroidRow[getRandomBadAsteroidColumn()] = -1;
-  if(Math.random() > 0.5) asteroidRow[getRandomBadAsteroidColumn()] = -1;
+  if (Math.random() > 0.5) asteroidRow[getRandomBadAsteroidColumn()] = -1;
   return asteroidRow;
 }
 
@@ -106,7 +104,7 @@ function shiftAsteroidsDown() {
   asteroids[0] = getAsteroidRow();
 }
 
-function showHearts(){
+function showHearts() {
   const canvas = document.getElementById("heartsCanvas");
   var ctx = canvas.getContext("2d");
   var redHeart = document.getElementById("Red Heart");
@@ -114,9 +112,9 @@ function showHearts(){
   ctx.drawImage(grayHeart, 20, 20, 50, 50);
   ctx.drawImage(grayHeart, 80, 20, 50, 50);
   ctx.drawImage(grayHeart, 140, 20, 50, 50);
-  if(lives >= 1) ctx.drawImage(redHeart, 20, 20, 50, 50);
-  if(lives >= 2) ctx.drawImage(redHeart, 80, 20, 50, 50);
-  if(lives >= 3) ctx.drawImage(redHeart, 140, 20, 50, 50);
+  if (lives >= 1) ctx.drawImage(redHeart, 20, 20, 50, 50);
+  if (lives >= 2) ctx.drawImage(redHeart, 80, 20, 50, 50);
+  if (lives >= 3) ctx.drawImage(redHeart, 140, 20, 50, 50);
 }
 
 function showAsteroids() {
@@ -130,23 +128,22 @@ function showAsteroids() {
       let y = 20 + i * 160;
       cellColor = "#e74c3c";
 
-      if(i == 3){
+      if (i == 3) {
         if (asteroids[i][j] === -1) {
           var img = document.getElementById("Faded Error Asteroid");
         } else {
           var img = document.getElementById("Faded Valid Asteroid");
         }
-        ctx.drawImage(img, x, y+100, 100, 100);
-      }
-      else{
+        ctx.drawImage(img, x, y + 100, 100, 100);
+      } else {
         if (asteroids[i][j] === -1) {
           var img = document.getElementById("Error Asteroid");
         } else {
           var img = document.getElementById("Valid Asteroid");
         }
         ctx.drawImage(img, x, y, 100, 100);
-    }
-      
+      }
+
       if (i == asteroids.length - 1 && j == currentPosition) {
         showRobot(x, y);
       }
