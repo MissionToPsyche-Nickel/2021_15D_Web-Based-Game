64bf208cdc0c82054753080133d8d93211179acb
Sophia Shovkovy <sophia.shovkovy@culdesac.com>
2020-11-08 19:33:54 -0700

    add new space background

--- asteroidsGame.js
+++ asteroidsGame.js
@@ -2,7 +2,7 @@
 //TODO: Embed Timer in Canvas
 //Set the canvas background to be the Space_Background photo
 document.getElementById("spaceCanvas").style.background =
-  "url('images/Space_Background_V2.png ')";
+  "url('images/Space_Background_V3.png ')";
 
 console.log("Starting...."); //sanity check console log
 
@@ -48,29 +48,31 @@ function handleKeyPress(event) {
 }
 
 function calcJump() {
-  if (asteroids[2][currentPosition] === 1) { //check if the asteroid about to be "jumped" to is a valid asteroid
-    score++; 
+  if (asteroids[2][currentPosition] === 1) {
+    //check if the asteroid about to be "jumped" to is a valid asteroid
+    score++;
     document.getElementById("score").innerHTML = "SCORE: " + score; //refresh the score in the HTML
     console.log("GOOD JUMP!");
-  } 
-  else { //asteroid jumped to an Error Asteroid (NOTE: if adding asteroids of alternate point value, this will need to be reworked)
+  } else {
+    //asteroid jumped to an Error Asteroid (NOTE: if adding asteroids of alternate point value, this will need to be reworked)
     lives--;
     console.log("UNSAFE ASTEROID JUMP!");
   }
 
   //No lives remaining
-  if(lives == 0) { 
+  if (lives == 0) {
     stop(); //stop the timer
     showHearts(); //refresh hearts
     setTimeout(function () {
-      if (confirm("GAME OVER! Play Again?")) { //user chose to Play Again
+      if (confirm("GAME OVER! Play Again?")) {
+        //user chose to Play Again
         startGame(); //restart the game
         document.getElementById("score").innerHTML = "SCORE: " + score; //refresh the score in the HTML
-      }
-      else { //if the user chose not to Play Again, disable robot movement
+      } else {
+        //if the user chose not to Play Again, disable robot movement
         document.removeEventListener("keydown", handleKeyPress);
       }
-    }, 30);//30ms wait to update hearts on canvas before displaying alert
+    }, 30); //30ms wait to update hearts on canvas before displaying alert
   }
 }
 
@@ -87,16 +89,16 @@ function startGame() {
   }
 
   //start the robot on a row of gray asteroids
-  asteroids.push([1, 1, 1]); 
+  asteroids.push([1, 1, 1]);
 
   //clear the canvas of any drawings from a previous game
   const canvas = document.getElementById("spaceCanvas");
   var ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  setTimeout(function() {
+  setTimeout(function () {
     showGame();
-    }, 30); //30ms timeout to make sure images are loaded
+  }, 30); //30ms timeout to make sure images are loaded
 }
 
 //returns a randomly generated integer array of 3 elements, where 1 is a valid asteroid and -1 is an Error Asteroid
@@ -112,7 +114,6 @@ function getRandomBadAsteroidColumn() {
   return Math.floor(Math.random() * Math.floor(3));
 }
 
-
 function shiftAsteroidsDownAndGetNewRow() {
   //Shifts all asteroids down one row
   for (let i = 3; i > 0; i--) {
@@ -121,11 +122,10 @@ function shiftAsteroidsDownAndGetNewRow() {
   asteroids[0] = getAsteroidRow(); //sets the top row to a new asteroid row
 }
 
-
 function showHearts() {
   var redHeart = document.getElementById("Red Heart"); //red heart image variable
-  var grayHeart = document.getElementById("Gray Heart");  //gray heart image variable
-  const canvas = document.getElementById("spaceCanvas");  //canvas variable
+  var grayHeart = document.getElementById("Gray Heart"); //gray heart image variable
+  const canvas = document.getElementById("spaceCanvas"); //canvas variable
   var ctx = canvas.getContext("2d"); //ctx is used to access the drawImage() functionality of the canvas
 
   //The drawImage() method works as following:
@@ -150,35 +150,36 @@ function showGame() {
   //iterate through the 2D Array
   for (let i = 0; i < asteroids.length; i++) {
     for (let j = 0; j < asteroids[i].length; j++) {
-      
       let x = 300 + j * 200; //X Coordinate to draw the asteroid, Start 300 to the right, and each column is 200 to the right of the last
       let y = 20 + i * 160; //Y Coordinate to draw the asteroid, Start 20 below the top of the screen, and each row is 160 below the last
- 
+
       //Drawing the incoming asteroids (rows 0, 1, and 2 )
       if (i != 3) {
-        if (asteroids[i][j] === -1) { //-1 means Error Asteroid
+        if (asteroids[i][j] === -1) {
+          //-1 means Error Asteroid
           var asteroidToDraw = document.getElementById("Error Asteroid");
-        } 
-        else { //Non -1 Value means Valid Asteroid
+        } else {
+          //Non -1 Value means Valid Asteroid
           var asteroidToDraw = document.getElementById("Valid Asteroid");
         }
         //The drawImage() method works as following:
-        //Note that here, x and y change each iteration to display the 2D grid 
+        //Note that here, x and y change each iteration to display the 2D grid
         //ctx.drawImage(image to display, x axis positon, y axis position, image width, image height)
-        ctx.drawImage(asteroidToDraw, x, y, 100, 100); 
+        ctx.drawImage(asteroidToDraw, x, y, 100, 100);
       }
 
       //Special Case: Drawing the outgoing asteroids using the Faded images
-      else{
-        if (asteroids[i][j] === -1) {//-1 means Error Asteroid
+      else {
+        if (asteroids[i][j] === -1) {
+          //-1 means Error Asteroid
           var asteroidToDraw = document.getElementById("Faded Error Asteroid");
-        } 
-        else {//Non -1 Value means Valid Asteroid
+        } else {
+          //Non -1 Value means Valid Asteroid
           var asteroidToDraw = document.getElementById("Faded Valid Asteroid");
         }
         //The drawImage() method works as following:
         //Note that here, x and y change each iteration to display the 2D grid
-        //An additional 100 is added to y to shift the outgoing asteroids underneath the robot simualting the "jump" 
+        //An additional 100 is added to y to shift the outgoing asteroids underneath the robot simualting the "jump"
         //ctx.drawImage(image to display, x axis positon, y axis position, image width, image height)
         ctx.drawImage(asteroidToDraw, x, y + 100, 100, 100);
       }
@@ -191,17 +192,20 @@ function showRobot(x, y) {
   var ctx = canvas.getContext("2d"); //ctx is used to access the drawImage() functionality of the canvas
   var robot = document.getElementById("Character"); //robot image variable
   ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the robot canvas
-  if (currentPosition == 0) { //draw the robot in the left col
+  if (currentPosition == 0) {
+    //draw the robot in the left col
     //The drawImage() method works as following:
     //ctx.drawImage(image to display, x axis positon, y axis position, image width, image height)
     ctx.drawImage(robot, 300, 500, 100, 100);
-  }         
-  if (currentPosition == 1) { //draw the robot in the middle col
+  }
+  if (currentPosition == 1) {
+    //draw the robot in the middle col
     //The drawImage() method works as following:
     //ctx.drawImage(image to display, x axis positon, y axis position, image width, image height)
     ctx.drawImage(robot, 500, 500, 100, 100);
   }
-  if (currentPosition == 2) { //draw the robot in the right col
+  if (currentPosition == 2) {
+    //draw the robot in the right col
     //The drawImage() method works as following:
     //ctx.drawImage(image to display, x axis positon, y axis position, image width, image height)
     ctx.drawImage(robot, 700, 500, 100, 100);
@@ -209,25 +213,25 @@ function showRobot(x, y) {
 }
 
 function tick() {
-    timeElapsed++
-    document.getElementById("time").innerHTML = "TIMER: " + timeElapsed;
+  timeElapsed++;
+  document.getElementById("time").innerHTML = "TIMER: " + timeElapsed;
 }
 
 function start() {
-    if(timerID == -1){
-        timerID = setInterval(tick, 1000);
-    }
+  if (timerID == -1) {
+    timerID = setInterval(tick, 1000);
+  }
 }
 
 function stop() {
-    if(timerID != -1){
-        clearInterval(timerID)
-        timerID = -1
-    }
+  if (timerID != -1) {
+    clearInterval(timerID);
+    timerID = -1;
+  }
 }
 
 function reset() {
-    stop();
-    timeElapsed = -1;
-    tick()
+  stop();
+  timeElapsed = -1;
+  tick();
 }
